5a039077761359d03db762496e4e3f85fd79e740
mitibirru <357miteshbirru@gmail.com>
2022-01-25 11:44:31 +0530

    Product catalog, cart using django sessions, custom context processor, manage customer orders

---
+++ orders/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+app_name = 'orders'
+
+urlpatterns = [
+  path('create/', views.order_create, name='order_create'),
+]
